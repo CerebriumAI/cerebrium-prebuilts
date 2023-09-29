98b6eb28aa7d032e9ff1efec616bc1c713a92aca
Michael Louis <michaellouis157@gmail.com>
2023-09-28 20:44:11 -0400

    Added mistral

---
+++ mistral-7b/main.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from typing import Optional
+
+import torch
+from pydantic import BaseModel
+from vllm import LLM, SamplingParams
+
+llm = LLM(model="mistralai/Mistral-7B-v0.1")
+
+class Item(BaseModel):
+    prompt: str
+    temperature: Optional[float] = 0.8
+    top_p: Optional[float] = 0.75
+    top_k: Optional[float] = 40
+    max_tokens: Optional[int] = 256
+    frequency_penalty: Optional[int] = 1
+
+def predict(item, run_id, logger):
+    item = Item(**item)
+    sampling_params = SamplingParams(temperature=item.temperature, top_p=item.top_p, top_k=item.top_k, max_tokens=item.max_tokens, frequency_penalty=item.frequency_penalty)
+    outputs = llm.generate([item.prompt], sampling_params)
+
+    generated_text = []
+    for output in outputs:
+        generated_text.append(output.outputs[0].text)
+    return {"result": generated_text}
