b65019203ec853d8daa4c28a0d2275880fc501b9
Michael Katsoulis <michaelkatsoulis@gmail.com>
2023-11-03 10:37:50 +0100

    better return for esrgan

--- esrgan-upscaler/main.py
+++ esrgan-upscaler/main.py
@@ -14,6 +14,7 @@ from realesrgan import RealESRGANer
 SOURCE = os.environ.get("SOURCE", "cortex")
 APP_NAME = os.environ.get("APP_NAME", "eseragan")
 
+
 #######################################
 # User-facing API Parameters
 #######################################
@@ -29,6 +30,7 @@ class Item(BaseModel):
 # Model Setup
 #######################################
 
+
 # Downloads a file from a given URL and saves it to a given filename
 def download_file_from_url(logger, url: str, filename: str):
     logger.info("Downloading file...")
@@ -42,7 +44,7 @@ def download_file_from_url(logger, url: str, filename: str):
 
     else:
         logger.info(response.text)
-        raise Exception("Download failed")
+        raise Exception(f"Download failed. Response from URL was: \nStatus Code:{response.status_code}\nText: {response.text}\nContent: {response.content}")
 
 
 # check if model is already in /persistent-storage
@@ -51,12 +53,16 @@ model_path_realESRGAN = os.path.join(model_dir, "RealESRGAN_x4plus.pth")
 if not os.path.exists(model_dir):
     os.makedirs(model_dir, exist_ok=True)
 if not os.path.exists(model_path_realESRGAN):
-    response = requests.get("https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth")
+    response = requests.get(
+        "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth"
+    )
     with open(model_path_realESRGAN, "wb") as f:
         f.write(response.content)
     print("Download complete")
 
-model = RRDBNet(num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4)
+model = RRDBNet(
+    num_in_ch=3, num_out_ch=3, num_feat=64, num_block=23, num_grow_ch=32, scale=4
+)
 upsampler = RealESRGANer(
     scale=4,
     model_path=model_path_realESRGAN,
@@ -69,7 +75,9 @@ upsampler = RealESRGANer(
 )
 model_path_GFPGANv1 = os.path.join(model_dir, "GFPGANv1.3.pth")
 if not os.path.exists(model_path_GFPGANv1):
-    response = requests.get("https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth")
+    response = requests.get(
+        "https://github.com/TencentARC/GFPGAN/releases/download/v1.3.0/GFPGANv1.3.pth"
+    )
     with open(model_path_GFPGANv1, "wb") as f:
         f.write(response.content)
     print("Download complete")
@@ -85,7 +93,9 @@ def predict(item, run_id, logger):
         image = download_file_from_url(logger, params.file_url, input_filename)
         init_image = cv2.imread(image, cv2.IMREAD_UNCHANGED)
     elif params.image is not None:
-        image_array = np.asarray(bytearray(BytesIO(base64.b64decode(params.image)).read()), dtype=np.uint8)
+        image_array = np.asarray(
+            bytearray(BytesIO(base64.b64decode(params.image)).read()), dtype=np.uint8
+        )
         init_image = cv2.imdecode(image_array, cv2.IMREAD_UNCHANGED)
     else:
         raise Exception("No image or file_url provided")
@@ -98,7 +108,9 @@ def predict(item, run_id, logger):
             channel_multiplier=2,
             bg_upsampler=upsampler,
         )
-        _, _, output = face_enhancer.enhance(init_image, has_aligned=False, only_center_face=False, paste_back=True)
+        _, _, output = face_enhancer.enhance(
+            init_image, has_aligned=False, only_center_face=False, paste_back=True
+        )
     else:
         output, _ = upsampler.enhance(init_image, outscale=params.upscale)
 
